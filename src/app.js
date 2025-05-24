9dac33527ebf75b6ed1eb338f0a4aa07953cee5d
kajal1698 <kajalsoni1698@gmail.com>
2025-05-24 16:26:59 +0530

    multiple route handler cases

--- src/app.js
+++ src/app.js
@@ -6,9 +6,82 @@ const app = express();
 app.use("/test",(req,res)=>{
     res.send("Hello From test");
 })
-app.use("/user",(req,res)=>{
-    res.send("All the user rest will display this msg because order matters");
-})
+// app.use("/user",(req,res)=>{
+//     //request handler 1
+//     console.log("request handler 1");
+//     res.send("handler 1 response");
+// },(req,res)=>{
+//     console.log("request handler 2");
+//     res.send("handler 2 response");
+// }
+// )
+
+//case 1
+
+// app.use("/user",(req,res,next)=>{
+//     //request handler 1
+//     console.log("request handler 1");
+//     res.send("handler 1 response");
+//     next();
+// },(req,res)=>{
+//     console.log("request handler 2");
+//     res.send("handler 2 response");
+// }
+// )
+
+//case 2
+
+// app.use("/user",(req,res,next)=>{
+//     //request handler 1
+//     console.log("request handler 1");
+//     next();
+//     res.send("handler 1 response");
+// },(req,res)=>{
+//     console.log("request handler 2");
+//     res.send("handler 2 response");
+// }
+// )
+
+//case 3
+
+// app.use("/user",(req,res,next)=>{
+//     //request handler 1
+//     console.log("request handler 1");
+//     next();
+//     // res.send("handler 1 response");
+// },(req,res)=>{
+//     console.log("request handler 2");
+//     res.send("handler 2 response");
+// }
+// )
+
+//case 4
+
+// app.use("/user",(req,res,next)=>{
+//     //request handler 1
+//     console.log("request handler 1");
+//     next();
+//     // res.send("handler 1 response");
+// },(req,res,next)=>{
+//     console.log("request handler 2");
+//     next();
+//     // res.send("handler 2 response");
+// }
+// )
+
+//case 5
+
+// app.use("/user",(req,res,next)=>{
+//     //request handler 1
+//     console.log("request handler 1");
+//     next();
+//     // res.send("handler 1 response");
+// },(req,res,next)=>{
+//     console.log("request handler 2");
+//     // next();
+//     // res.send("handler 2 response");
+// }
+// )
 //this will only handle GET call to /user
 app.get("/user",(req,res)=>{
     res.send({firstName : "Kajal",lastName: "Soni"});
@@ -22,9 +95,9 @@ app.use("/hello",(req,res)=>{
 app.use("/hello/2",(req,res)=>{
     res.send("hello from 2");
 })
-app.use("/",(req,res)=>{
-    res.send("Hello From home page");
-})
+// app.use("/",(req,res)=>{
+//     res.send("Hello From home page");
+// })
 app.listen(5000,()=>{
     console.log("Server is listening on port 5000");
 })
